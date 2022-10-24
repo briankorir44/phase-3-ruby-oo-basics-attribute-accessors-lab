345d6ebec765709f9bb4de0b93040736fcfc5c46
briankorir44 <briankorir44@yahoo.com>
2022-10-24 10:46:56 +0300

    complete lab

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,15 @@
 ## code your solution here. 
+class Cat
+    attr_accessor :name
+    def meow
+        puts "meow!"
+end
+end
+maru = Cat.new
+maru.name = "maru"
+pp maru.meow
+
+# Cat.name = "meow!"
+# Cat.name
+# end
+
